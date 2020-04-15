741135229a9d21d66ddbaf99d6d39c4989c8c209
Imtiaz Malik <mimtiaz@pionlog.com>
2020-04-15 23:13:17 +0500

    Imtiaz's Work

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Form from './Form'
 import HomeRoute from './HomeRoute'
 import AboutRoute from './AboutRoute'
 import Employees from './Employees'
-import EmployeesByHook from './EmployeesByHook'
-import FormByHooks from './FormByHooks'
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 
 function App() {
@@ -18,18 +16,16 @@ function App() {
               <div class="col-lg-8">
               <Router>
                 <div class="row">
-                    <div class="col-lg-2"><Link to="/">Home </Link></div>
-                    <div class="col-lg-2"><Link to="/about">About </Link></div>
-                    <div class="col-lg-2"><Link to="/form">Form </Link></div>
+                    <div class="col-lg-3"><Link to="/">Home </Link></div>
+                    <div class="col-lg-3"><Link to="/about">About </Link></div>
+                    <div class="col-lg-3"><Link to="/form">Form </Link></div>
                     <div class="col-lg-3"><Link to="/employees">Employees </Link></div>
-                    <div className="col-lg-4"><Link to="/EmployeesByHook"> Get Employees by Hooks </Link></div>
                 </div>
                 <br/>
                 <Route exact path="/" component={HomeRoute} />
                 <Route path="/about" component={AboutRoute} />
                 <Route path="/form" component={Form} />
                 <Route path="/employees" component={Employees} />
-                <Route path="/EmployeesByHook" component={EmployeesByHook} />
               </Router>
               </div>
               
--- src/Employees.js
+++ src/Employees.js
@@ -1,27 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-class Employees extends React.Component
-{
-    constructor()
-    {
-        super()
-        this.state={
-            employee:null
-        }
-    }
-    componentDidMount()
-    {
-        fetch('http://dummy.restapiexample.com/api/v1/employees').then((response)=>{
-            response.json().then(result=>{
-                // console.warn(response);
-                this.setState({employee:result.data})
-            })
+function Form({addEmployee}){
+    const [name,setName] =  useState('');
+    const [age,setAge] =  useState('');
+    const [salary,setSalary] =  useState('');
+  
+    const handleSubmit = e =>{
+    e.preventDefault();
+    if(!name) return;
+    addEmployee(name,age,salary);
+    let url="http://dummy.restapiexample.com/api/v1/create"
+    let data={'name':name,'age':age,'salary':salary}
+    fetch(url,{
+        method: 'POST',
+        headers:{
+            "Content-type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify(data)
+    }).then((result)=>{
+        result.json().then((resp)=>{
+            alert('Employee data is submitted:' + JSON.stringify(resp)); 
         })
-    }
-    render()
-    {
+    })
+      setName('');
+      setAge('');
+      setSalary('');
+    };
+  
+    return(
+            <div>
+                <div className="container">
+                    <h2 className="text-center">Add an Employee</h2>
+                    <form onSubmit={handleSubmit}>
+                    <input className="form-control" type="text" value={name}  placeholder="Enter your name" onChange = {e => setName(e.target.value)} />        
+                    <input className="form-control" type="number" value={salary}   placeholder="Enter employee salary" onChange = {e => setSalary(e.target.value)} ></input>
+                    <input className="form-control" type="number" value={age}   placeholder="Enter employee age" onChange = {e => setAge(e.target.value)} ></input>
+                              
+                    <button  className="btn margin-top btn-success" type="submit" >Add</button>
+                    </form>
+                </div>
+            </div>
+        )
+  }
+
+function Employees()
+{
+    const [employees, setEmployees] = useState({ data: [] });
+    const [emp,setEmp] = useState('');
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios(
+            'http://dummy.restapiexample.com/api/v1/employees',
+          );
+          setEmployees(result.data);
+        };
+        fetchData();
+      }, []);
+
+      const addEmployee = (name,age,salary) =>{
+        const newEmployee = [...emp,{name,salary,age}];
+        setEmp(newEmployee);
+      };
+    
         return(
             <div class="container">
+                <Form addEmployee={addEmployee}></Form>
                 <h1 class="text-center">All Employees Data</h1>
                 <br/>
                 <table class="table">
@@ -31,20 +76,18 @@ class Employees extends React.Component
                         <th scope="col">employee name</th>
                         <th scope="col">employee salary</th>
                         <th scope="col">age</th>
-                        <th>Edit</th>
                         <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            this.state.employee ?
-                            this.state.employee.map((employees,e)=>
+                            employees.data ?
+                            employees.data.map((employee,index)=>
                                 <tr>
-                                <th scope="row">{e}</th>
-                                <td>{employees.employee_name}</td>
-                                <td>$ {employees.employee_salary}</td>
-                                <td>{employees.employee_age}</td>
-                                <td><button class="btn btn-primary">Edit</button></td>
+                                <th scope="row">{index}</th>
+                                <td>{employee.employee_name}</td>
+                                <td>$ {employee.employee_salary}</td>
+                                <td>{employee.employee_age}</td>
                                 <td><button class="btn btn-danger">Delete</button></td>
                                 </tr>
                             )
@@ -53,9 +96,9 @@ class Employees extends React.Component
                         }
                     </tbody>
                 </table>
+                
             </div>
         )
-    }
 }
 
 export default Employees;
